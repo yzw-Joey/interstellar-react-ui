164fdb789dcae311e515539699df43db676d8832
杨智文 <yangzhiwen@192.168.2.3>
2024-07-15 22:07:25 +0800

    feat: add umd pkg

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    index: ['./src/index.ts'],
+  },
+  output: {
+    filename: 'interstellar-react-ui.js',
+    path: path.join(__dirname, 'dist/umd'),
+    library: 'Interstellar',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.build.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    dayjs: 'dayjs',
+  },
+};
