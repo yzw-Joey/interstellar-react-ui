0da4a952e625b9cc10214d7d12442c7555044132
杨智文 <yangzhiwen@192.168.2.3>
2024-07-15 22:22:01 +0800

    feat: add sass compile

---
+++ scripts/compileSass.sh
@@ -0,0 +1,7 @@
+npx sass ./src/Calendar/index.scss ./dist/esm/Calendar/index.css
+
+npx sass ./src/Calendar/index.scss ./dist/cjs/Calendar/index.css
+
+npx sass ./src/Message/index.scss ./dist/esm/Message/index.css
+
+npx sass ./src/Message/index.scss ./dist/cjs/Message/index.css
